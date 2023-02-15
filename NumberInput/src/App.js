33ac68e6caef134e127c9f83ad1ba977fd2359d6
terrancito <111590916+terrancito@users.noreply.github.com>
2023-02-14 23:28:15 -0300

    Add files via upload

---
+++ NumberInput/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import NumberInput from "./components/NumberInput/NumberInput";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <NumberInput />
+    </div>
+  );
+}
